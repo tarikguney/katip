521ec937188f8cd4f6d3be36e3503a13711752f2
Tarik Guney <atarikguney@gmail.com>
2017-09-05 00:34:45 -0600

    Implemented the bare bones of the CLI without their implementation and introduced a new package called commands

---
+++ commands/commands.go
@@ -0,0 +1,57 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli"
+)
+
+func GetAllCommands() []cli.Command {
+	return []cli.Command{
+		{
+			Name:    "watch",
+			Aliases: []string{"w"},
+			Usage:   "Starts a new watch. By default, it commits changes to the history as any change occurs, and folderpath is the current directory.",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Not yet implemented: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "stop",
+			Aliases: []string{"s"},
+			Usage:   "Stops the watch.",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Not yet implemented: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "destroy",
+			Aliases: []string{"d"},
+			Usage:   "Destroys all the history recorded. It also stops the application if it is still running.",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Not yet implemented: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "pause",
+			Aliases: []string{"p"},
+			Usage:   "Pauses the app.",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Not yet implemented: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "resume",
+			Aliases: []string{"d"},
+			Usage:   "Resumes the app with all the arguments passed initially when the app was first started.",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Not yet implemented: ", c.Args().First())
+				return nil
+			},
+		},
+	}
+}
--- main.go
+++ main.go
@@ -1,30 +1,19 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"os/exec"
+	"katip/commands"
+	"os"
 
 	"github.com/urfave/cli"
 )
 
-// I think the reason why this pkg folder exists is because it contains pre-compiled object files
-// that are not going to be compiled again for speed during the whole compilation process.
 func main() {
-	fmt.Println("Hello, World")
 	app := cli.NewApp()
 	app.Name = "Katip"
 	app.Usage = "Micro backups with historical changes"
-	command := exec.Command("git", "log")
-	var out bytes.Buffer
+	app.Version = "0.0.1"
 
-	command.Stdout = &out
-	var err error = command.Run()
-	// app.Action = func(context *cli.Context) error {
-	// 	fmt.Println("Hello, World")
-	// 	return nil
-	// }
-	fmt.Println(out.String())
-	fmt.Println(err)
-	// app.Run(os.Args)
+	app.Commands = commands.GetAllCommands()
+
+	app.Run(os.Args)
 }
