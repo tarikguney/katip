bb29cfb89382e29b49be1cf7221ec3d42e87c76e
Tarik Guney <atarikguney@gmail.com>
2018-12-25 14:00:05 -0700

    Implementing tracking logic

--- commands/watch-command.go
+++ commands/watch-command.go
@@ -11,6 +11,7 @@ import (
 
 func runWatchCommand() (success bool, err error) {
 	_, fileError := os.Stat(common.KATIP_REPO)
+
 	if !os.IsNotExist(fileError) {
 		err = errors.New("this folder is already being watched. i will continue adding to the existing history")
 		success = false
@@ -23,24 +24,23 @@ func runWatchCommand() (success bool, err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer watcher.Close()
 
 	done := make(chan bool)
 	go func() {
 		for {
 			select {
-					case event, ok := <-watcher.Events:
-						if !ok {
-						return
-					}
-						log.Println("Event:", event)
-						if event.Op&fsnotify.Write == fsnotify.Write {
-						log.Println("Modified file:" + event.Name)
-					}
-
-					case err, ok := <-watcher.Errors:
-						if !ok {
-						return
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op == fsnotify.Write {
+					log.Println("Modified file:" + event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
 				}
 				log.Println("Error:", err)
 			}
